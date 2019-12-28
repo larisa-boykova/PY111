9130d751af3a5bc1b5474300b26f5e19df0b900c
Larisa Boykova <larisa_boykova@mail.ru>
2019-12-28 13:12:28 +0300

    бинарный поиск

--- Tasks/b1_binary_search.py
+++ Tasks/b1_binary_search.py
@@ -9,5 +9,21 @@ def binary_search(elem: Any, arr: Sequence) -> Optional[int]:
 	:param arr: array where element is to be found
 	:return: Index of element if it's presented in the arr, None otherwise
 	"""
-	print(elem, arr)
-	return None
+	ind_start = 0
+	ind_stop = len(arr)
+
+	while True:
+		if ind_start==ind_stop:
+			return None
+		mid_ind = int((ind_stop - ind_start) / 2)+ind_start
+		if elem == arr[mid_ind]:
+			return mid_ind
+
+		if elem < arr[mid_ind]:
+			ind_stop = mid_ind
+
+		if elem > arr[mid_ind]:
+			ind_start = mid_ind + 1
+
+
+binary_search (12,[0,3,5,9,11,24,42])
